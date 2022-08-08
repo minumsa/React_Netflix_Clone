8cd71474b51f1dc720cf8a1a344caa7bbfd6d637
유일 <83910706+minumsa@users.noreply.github.com>
2022-08-08 11:42:33 +0900

    Styled Component를 이용해서 Footer 생성하기

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Row from './components/Row';
 import React from 'react';
 import Banner from './components/Banner';
 import requests from './api/requests';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
         id="CM"
         fetchUrl={requests.fetchComedyMovies}
       />
+      <Footer />
     </div>
   );
 }
