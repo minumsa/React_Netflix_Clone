0213015932b64d06f95265ba878950c7935eadd0
minumsa <icycyi@naver.com>
2022-08-09 16:48:22 +0900

    github를 이용해서 배포하기

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "dc934f33eb2690e524e2021cad25d59e",
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
     language: "ko-KR",
   },
 });
