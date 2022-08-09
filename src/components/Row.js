6e928f3a927a548264f795d1d35c4844df25cad9
minumsa <icycyi@naver.com>
2022-08-09 16:41:28 +0900

    swiper 모듈을 이용한 터치 슬라이드 구현하기

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import axios from '../api/axios';
-import './Row.css';
-import MovieModal from './MovieModal';
+import React, { useEffect, useState } from "react";
+import axios from "../api/axios";
+import "./Row.css";
+import MovieModal from "./MovieModal";
+
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 export default function Row({ isLargeRow, title, id, fetchUrl }) {
   const [movies, setMovies] = useState([]);
@@ -14,11 +24,11 @@ export default function Row({ isLargeRow, title, id, fetchUrl }) {
 
   const fetchMovieData = async () => {
     const request = await axios.get(fetchUrl);
-    console.log('request', request);
+    console.log("request", request);
     setMovies(request.data.results);
   };
 
-  const handleClick = (movie) => {
+  const handleClick = movie => {
     setModalOpen(true);
     setMovieSelected(movie);
   };
@@ -27,44 +37,50 @@ export default function Row({ isLargeRow, title, id, fetchUrl }) {
     <section className="row">
       {/** TITLE */}
       <h2>{title}</h2>
-      <div className="slider">
-        <div className="slider__arrow-left">
-          <span
-            className="arrow"
-            onClick={() => {
-              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-            }}
-          >
-            {'<'}
-          </span>
-        </div>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        navigation
+        pagination={{ clickable: true }}
+        loop={true}
+        breakpoints={{
+          1378: {
+            slidesPerView: 6,
+            slidesPerGroup: 6,
+          },
+          998: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }}
+      >
         <div id={id} className="row__posters">
           {/**SEVERAL ROW__POSTER */}
           {movies &&
-            movies.map((movie) => (
-              <img
-                key={movie.id}
-                className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
-                src={`https://image.tmdb.org/t/p/original/${
-                  isLargeRow ? movie.poster_path : movie.backdrop_path
-                } `}
-                loading="lazy"
-                alt={movie.name}
-                onClick={() => handleClick(movie)}
-              />
+            movies.map(movie => (
+              <SwiperSlide>
+                <img
+                  key={movie.id}
+                  className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                  src={`https://image.tmdb.org/t/p/original/${
+                    isLargeRow ? movie.poster_path : movie.backdrop_path
+                  } `}
+                  loading="lazy"
+                  alt={movie.name}
+                  onClick={() => handleClick(movie)}
+                />
+              </SwiperSlide>
             ))}
         </div>
-        <div className="slider__arrow-right">
-          <span
-            className="arrow"
-            onClick={() => {
-              document.getElementById(id).scrollLeft += window.innerWidth - 80;
-            }}
-          >
-            {'>'}
-          </span>
-        </div>
-      </div>
+      </Swiper>
 
       {modalOpen && (
         <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
